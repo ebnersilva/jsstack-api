939ae0b9a3fa5885e9ac45a11d90e2d5d8211f33
Ebner Silva <ebnersilva@MacBook-Pro-de-Ebner.local>
2023-03-04 23:31:01 -0300

    feat: add eslint and editorconfig

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (request, response) => {
-  response.send('Hello World!')
-})
+  response.send('Hello World!');
+});
 
-app.listen(3000, () => console.log('🔥 Server started at port 3000'))
+app.listen(3000, () => console.log('🔥 Server started at port 3000'));
