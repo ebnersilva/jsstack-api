fc9fe4f6e56412091657bcfd05c38e77665cca9e
ebnersilva <ebner.suporte@hotmail.com>
2023-03-07 14:31:01 -0300

    feat: add feature to create a contact

--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ const routes = require('./routes');
 
 const app = express();
 
-app.use(routes);
+app.use(express.json());
 
+app.use(routes);
 
 app.listen(3000, () => console.log('🔥 Server started at port 3000'));
