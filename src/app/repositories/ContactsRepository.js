55176e08fd7676244247fa1e9cda97af1194e254
ebnersilva <ebner.suporte@hotmail.com>
2023-03-08 08:52:09 -0300

    feat: integrate Repository update and delete methods with pg database

--- src/app/repositories/ContactsRepository.js
+++ src/app/repositories/ContactsRepository.js
@@ -1,23 +1,4 @@
-const { v4 } = require('uuid');
-
 const db = require('../database');
-
-let contacts = [
-  {
-    id: v4(),
-    name: 'Ebner',
-    email: 'ebner.suporte@hotmail.com',
-    phone: '123123123',
-    category_id: v4(),
-  },
-  {
-    id: v4(),
-    name: 'Jenifer',
-    email: 'jenifer@hotmail.com',
-    phone: '123123123',
-    category_id: v4(),
-  },
-];
 
 class ContactsRepository {
   async findAll(orderBy = 'ASC') {
@@ -40,12 +21,10 @@ class ContactsRepository {
     return row;
   }
 
-  delete(id) {
-    return new Promise((resolve) => {
-      contacts = contacts.filter((contact) => contact.id !== id);
+  async delete(id) {
+    const deleteOp = await db.query('DELETE FROM contacts WHERE id = $1', [id]);
 
-      resolve();
-    });
+    return deleteOp;
   }
 
   async create({
@@ -63,28 +42,17 @@ class ContactsRepository {
     return row;
   }
 
-  update(id, {
+  async update(id, {
     name, email, phone, category_id,
   }) {
-    return new Promise((resolve) => {
-      const updatedContact = {
-        id,
-        name,
-        email,
-        phone,
-        category_id,
-      };
+    const [row] = await db.query(`
+      UPDATE contacts
+      SET name = $1, email = $2, phone = $3, category_id = $4
+      WHERE id = $5
+      RETURNING *
+    `, [name, email, phone, category_id, id]);
 
-      contacts = contacts.map((contact) => {
-        if (contact.id === id) {
-          return updatedContact;
-        }
-
-        return contact;
-      });
-
-      resolve(updatedContact);
-    });
+    return row;
   }
 }
 
